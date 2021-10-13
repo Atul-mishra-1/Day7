5e28dd5c85371f6bb883e5ccb42f737f96be8b97
Atul-mishra-1 <52456162+Atul-mishra-1@users.noreply.github.com>
2021-10-13 15:26:16 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,14 @@
+package str;
+
+//program to test if a given string contains the specified sequence of char values.
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1= "test char sequence";
+		String testSequence= "char";
+		System.out.println(str1.contains(testSequence));
+	}
+
+}
